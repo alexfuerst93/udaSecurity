48bff427e69abb4cd4f8935098ae1454a7f89868
alexander fürst <alexander.fuerst@fuerst-autoteile.de>
2022-02-17 12:02:00 +0100

    restructured project to into 2 modules

---
+++ securityService/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.udacity.catpoint.securityService {
+    requires com.udacity.catpoint.imageService;
+    requires java.desktop;
+    requires miglayout;
+    requires com.google.common;
+    requires com.google.gson;
+    requires java.prefs;
+    opens com.udacity.catpoint.security.data to com.google.gson;
+}
