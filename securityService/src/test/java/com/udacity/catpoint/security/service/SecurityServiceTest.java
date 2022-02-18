102f8888e42c63bb1b919fc1b389ceb72da292df
alexander fürst <alexander.fuerst@fuerst-autoteile.de>
2022-02-18 09:33:03 +0100

    refactored unittests to use Mockito

--- securityService/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
+++ securityService/src/test/java/com/udacity/catpoint/security/service/SecurityServiceTest.java
@@ -10,70 +10,97 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-class SecurityServiceTest {
+public class SecurityServiceTest {
 
-    SecurityService securityService;
-    SecurityRepository testData;
+    private SecurityService securityService;
 
     @Mock
-    FakeImage fakeImage;
+    private FakeImage fakeImage;
+
+    @Mock
+    private PretendDatabaseSecurityRepositoryImpl newTestData;
 
     @BeforeEach
     void init() {
-        // ensures system is always: 'AlarmStatus.NO_ALARM & ArmingStatus.DISARMED' when initiated
-        // furthermore, all sensors initialized are true/active
-        testData = new PretendDatabaseSecurityRepositoryImpl();
-        testData.setAlarmStatus(AlarmStatus.NO_ALARM);
-        testData.setArmingStatus(ArmingStatus.DISARMED);
-    }
-
-    @Test
-    void internal_confirmsCleanSystemStart_SystemIsNoAlarmAndDisarmed() {
-        assertAll(
-                () -> assertEquals(AlarmStatus.NO_ALARM, testData.getAlarmStatus()),
-                () -> assertEquals(ArmingStatus.DISARMED, testData.getArmingStatus())
-        );
+        securityService = new SecurityService(newTestData, fakeImage);
     }
 
     @Test
     public void changeSensorActivationStatus_alarmArmedAndSensorActivated_returnPendingAlarm() {
     // 1. Requirement: If alarm is armed and a sensor becomes activated, put the system into pending alarm status
-        testData.setArmingStatus(ArmingStatus.ARMED_HOME);
-        // TODO: Add the second option!
-        //testData.setArmingStatus(ArmingStatus.ARMED_AWAY);
         Sensor sensor = new Sensor("testSensor", SensorType.DOOR);
         Boolean sensorState = true;
 
-        securityService = new SecurityService(testData, fakeImage);
+        when(newTestData.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
+        // TODO: Add the second option!
+        //testData.setArmingStatus(ArmingStatus.ARMED_AWAY);
+        when(newTestData.getAlarmStatus()).thenReturn(AlarmStatus.NO_ALARM);
+
+        securityService.addSensor(sensor);
         securityService.changeSensorActivationStatus(sensor, sensorState);
-        assertEquals(AlarmStatus.PENDING_ALARM, securityService.getAlarmStatus());
+
+        verify(newTestData).setAlarmStatus(AlarmStatus.PENDING_ALARM);
+        //assertEquals(AlarmStatus.PENDING_ALARM, securityService.getAlarmStatus()); -> Doesnt work because return type is void
     }
 
     @Test
     public void what_method_am_I_testing() {
         // 2. Requirement: If alarm is armed and a sensor becomes activated and the system is already pending alarm, set the alarm status to alarm.
-        testData.setArmingStatus(ArmingStatus.ARMED_HOME);
-        // TODO: Add the second option!
-        //testData.setArmingStatus(ArmingStatus.ARMED_AWAY);
         Sensor sensor = new Sensor("testSensor", SensorType.DOOR);
         Boolean sensorState = true;
-        testData.setAlarmStatus(AlarmStatus.PENDING_ALARM);
 
-        securityService = new SecurityService(testData, fakeImage);
+        when(newTestData.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
+        // TODO: Add the second option!
+        //testData.setArmingStatus(ArmingStatus.ARMED_AWAY);
+        when(newTestData.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
+
+        securityService.addSensor(sensor);
         securityService.changeSensorActivationStatus(sensor, sensorState);
-        assertEquals(AlarmStatus.ALARM, securityService.getAlarmStatus());
+        verify(newTestData).setAlarmStatus(AlarmStatus.ALARM);
     }
 
     @Test
     public void something() {
-        //If pending alarm and all sensors are inactive, return to no alarm state.
-        testData.setAlarmStatus(AlarmStatus.PENDING_ALARM);
-        for (Sensor sensor : testData.getSensors()) {
-            System.out.println(sensor.getActive());
-        }
+        // 3. Requirement: If pending alarm and all sensors are inactive, return to no alarm state.
+        Sensor sensor = new Sensor("testSensor", SensorType.DOOR);
+        Boolean sensorState = true;
 
-        assertEquals(AlarmStatus.NO_ALARM, securityService.getAlarmStatus());
+        when(newTestData.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
+        // TODO: Add the second option!
+        when(newTestData.getAlarmStatus()).thenReturn(AlarmStatus.PENDING_ALARM);
+
+        securityService.addSensor(sensor);
+        securityService.changeSensorActivationStatus(sensor, sensorState);
+        securityService.changeSensorActivationStatus(sensor, false);
+
+        verify(newTestData).setAlarmStatus(AlarmStatus.NO_ALARM);
+    }
+
+    @Test
+    public void something_else() {
+        // 4. If alarm is active, change in sensor state should not affect the alarm state.
+        Sensor sensor = new Sensor("testSensor", SensorType.DOOR);
+        Boolean sensorState = true;
+
+        when(newTestData.getArmingStatus()).thenReturn(ArmingStatus.ARMED_HOME);
+        // TODO: Add the second option!
+        when(newTestData.getAlarmStatus()).thenReturn(AlarmStatus.ALARM);
+
+        securityService.addSensor(sensor);
+        securityService.changeSensorActivationStatus(sensor, sensorState);
+        securityService.changeSensorActivationStatus(sensor, false);
+
+        // Is my test false or the code?
+        // My understanding: Once the Alarm fires, even deactivated sensors won't change the status
+        verify(newTestData, never()).setAlarmStatus(AlarmStatus.PENDING_ALARM);
+    }
+
+    @Test
+    public void yet_again_something_else() {
+        // 5. Requirement: If a sensor is activated while already active and the system is in pending state, change it to alarm state.
+
     }
 }
